8df096f408cc0c91c98c300404f5a0d8f88c2f83
Olumuyiwa <olumuyiwa.folorunso@gmail.com>
2026-09-12 10:19:21 +0100

    fix(build stage)

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,6 +1,7 @@
+//packages/app/src/App.tsx
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
-import { kubernetesPlugin } from '@backstage/plugin-kubernetes/alpha';
+import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha'; // <-- Changed to default import
 import { navModule } from './modules/nav';
 import { homeModule } from './modules/home';
 
