4016c0304ab15c7830dcbc466c9793066d513594
Olumuyiwa <olumuyiwa.folorunso@gmail.com>
2026-09-12 04:39:42 +0100

    k8s changes and frontend modifications

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,8 +1,9 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import { kubernetesPlugin } from '@backstage/plugin-kubernetes/alpha';
 import { navModule } from './modules/nav';
 import { homeModule } from './modules/home';
 
 export default createApp({
-  features: [catalogPlugin, navModule, homeModule],
+  features: [kubernetesPlugin, catalogPlugin, navModule, homeModule],
 });
--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -2,5 +2,15 @@ import '@backstage/cli/asset-types';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@backstage/ui/css/styles.css';
+import { v4 as uuidv4 } from 'uuid';
+
+if (typeof globalThis.crypto === 'undefined') {
+  (globalThis as any).crypto = {};
+}
+
+if (typeof globalThis.crypto.randomUUID !== 'function') {
+  globalThis.crypto.randomUUID = () => uuidv4() as `${string}-${string}-${string}-${string}-${string}`;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(App.createRoot());
+
